10752219998e8ee6911fbf06c3ab5c76e15ff00c
hoshogi <hoshogi@gmail.com>
2024-09-23 16:23:41 +0900

    #4 [feat] : Header 컴포넌트 dark mode 적용

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -5,7 +5,7 @@ const LeftHeader: React.FC = () => {
     <div>
       <a
         href="/"
-        className="from-redorange to-orange bg-gradient-to-b bg-clip-text text-2xl font-bold text-transparent"
+        className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text text-2xl font-bold text-transparent"
       >
         Owing.
       </a>
@@ -16,13 +16,13 @@ const LeftHeader: React.FC = () => {
 const RightHeader: React.FC = () => {
   return (
     <nav className="flex items-center space-x-12">
-      <a className="from-redorange to-orange bg-gradient-to-b bg-clip-text font-bold text-transparent">
+      <a className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text font-bold text-transparent">
         How to use
       </a>
-      <a className="from-redorange to-orange bg-gradient-to-b bg-clip-text font-bold text-transparent">
+      <a className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text font-bold text-transparent">
         Login
       </a>
-      <button className="from-redorange to-orange rounded-full bg-gradient-to-r px-6 py-2 font-bold text-white">
+      <button className="from-redorange to-orange dark:from-blue dark:to-skyblue rounded-full bg-gradient-to-r px-6 py-2 font-bold text-white">
         Contact us
       </button>
     </nav>
@@ -31,7 +31,7 @@ const RightHeader: React.FC = () => {
 
 const Header: React.FC = () => {
   return (
-    <header className="inset-0 flex h-auto w-full items-center justify-between bg-white px-8 py-4">
+    <header className="dark:bg-darkblack inset-0 flex h-auto w-full items-center justify-between bg-white px-8 py-4">
       <LeftHeader />
       <RightHeader />
     </header>
