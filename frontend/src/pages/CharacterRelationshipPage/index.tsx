320a25c3e78efbe5af9d6d25e71dbda1b72c5dcf
yunjeong <dbswjd1413@naver.com>
2024-09-23 18:01:26 +0900

    - tab toggle 남는 오류 해결 - react-flow 관련 컴포넌트 생성(node) - characterStore 생성

--- frontend/src/pages/CharacterRelationshipPage/index.tsx
+++ frontend/src/pages/CharacterRelationshipPage/index.tsx
@@ -1,62 +1,35 @@
 import React from 'react'
 
-import { useCustomReactFlow } from '@/hooks/useCustomReactFlow'
-import AddButton from '@/pages/CharacterRelationshipPage/AddButton'
-import { FlowEdge } from '@/types/node'
-import ReactFlow, {
-  Background,
-  Controls,
-  Edge,
-  MarkerType,
-  ReactFlowProvider,
-} from 'reactflow'
+import CustomNode from './CustomNode'
+
+import { useCharacterStore } from '@/stores/characterStore'
+import ReactFlow, { Background, Controls } from 'reactflow'
 import 'reactflow/dist/style.css'
 
-const FlowDiagramContent: React.FC = () => {
-  const {
-    nodes,
-    edges,
-    onNodesChange,
-    onEdgesChange,
-    onConnect,
-    handleAddNode,
-  } = useCustomReactFlow()
+const nodeTypes = {
+  custom: CustomNode,
+}
 
-  const edgeWithRelationship = (edge: FlowEdge): Edge => ({
-    ...edge,
-    label: edge.data?.relationship,
-    labelStyle: { fill: '#000', fontWeight: 700 },
-    markerEnd: { type: MarkerType.ArrowClosed, color: '#000' },
-  })
+const CharacterRelationship: React.FC = () => {
+  const { nodes, edges, onNodesChange, onEdgesChange, onConnect } =
+    useCharacterStore()
 
   return (
-    <div className="relative h-full w-full">
+    <div className="h-screen w-full">
       <ReactFlow
         nodes={nodes}
-        edges={edges.map(edgeWithRelationship)}
+        edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        nodeTypes={nodeTypes}
         fitView
       >
         <Background />
         <Controls />
       </ReactFlow>
-      <div className="absolute right-4 top-4 z-10">
-        <AddButton onClick={handleAddNode} />
-      </div>
     </div>
   )
 }
 
-const FlowDiagram: React.FC = () => {
-  return (
-    <ReactFlowProvider>
-      <div className="h-full w-full">
-        <FlowDiagramContent />
-      </div>
-    </ReactFlowProvider>
-  )
-}
-
-export default FlowDiagram
+export default CharacterRelationship
