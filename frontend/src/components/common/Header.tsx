1a21a3fb7f2a97456554a9fd5b133ce51dee1bda
hoshogi <hoshogi@gmail.com>
2024-09-23 16:16:40 +0900

    #4 [feat] : Header 컴포넌트 설계

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,5 +1,41 @@
-const Header = () => {
-  return <div className="flex justify-end">Header</div>
+import React from 'react'
+
+const LeftHeader: React.FC = () => {
+  return (
+    <div>
+      <a
+        href="/"
+        className="from-redorange to-orange bg-gradient-to-b bg-clip-text text-2xl font-bold text-transparent"
+      >
+        Owing.
+      </a>
+    </div>
+  )
+}
+
+const RightHeader: React.FC = () => {
+  return (
+    <nav className="flex items-center space-x-12">
+      <a className="from-redorange to-orange bg-gradient-to-b bg-clip-text font-bold text-transparent">
+        How to use
+      </a>
+      <a className="from-redorange to-orange bg-gradient-to-b bg-clip-text font-bold text-transparent">
+        Login
+      </a>
+      <button className="from-redorange to-orange rounded-full bg-gradient-to-r px-6 py-2 font-bold text-white">
+        Contact us
+      </button>
+    </nav>
+  )
+}
+
+const Header: React.FC = () => {
+  return (
+    <header className="inset-0 flex h-auto w-full items-center justify-between bg-white px-8 py-4">
+      <LeftHeader />
+      <RightHeader />
+    </header>
+  )
 }
 
 export default Header
