562098c8ff4df90e9a6f9ed85ed8614cbf4d5f58
hoshogi <hoshogi@gmail.com>
2024-09-23 19:02:43 +0900

    #4 [feat] : Header 컴포넌트 수정
    - Link 태그로 수정

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const LeftHeader: React.FC = () => {
   return (
     <div>
-      <a
-        href="/"
+      <Link
+        to="/"
         className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text text-2xl font-bold text-transparent"
       >
         Owing.
-      </a>
+      </Link>
     </div>
   )
 }
@@ -16,12 +18,18 @@ const LeftHeader: React.FC = () => {
 const RightHeader: React.FC = () => {
   return (
     <nav className="flex items-center space-x-12">
-      <a className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text font-bold text-transparent">
+      <Link
+        to="/"
+        className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text font-bold text-transparent"
+      >
         How to use
-      </a>
-      <a className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text font-bold text-transparent">
+      </Link>
+      <Link
+        to="/"
+        className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text font-bold text-transparent"
+      >
         Login
-      </a>
+      </Link>
       <button className="from-redorange to-orange dark:from-blue dark:to-skyblue rounded-full bg-gradient-to-r px-6 py-2 font-bold text-white">
         Contact us
       </button>
