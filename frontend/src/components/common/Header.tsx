dfd37310a63d10bdbce590dce73f51679e730c68
hoshogi <hoshogi@gmail.com>
2024-09-24 16:52:33 +0900

    #13 [feat] : Header 컴포넌트 다크모드 수정
    - 버튼 글씨 색깔 수정

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -7,7 +7,7 @@ const LeftHeader: React.FC = () => {
     <div>
       <Link
         to="/"
-        className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text text-2xl font-bold text-transparent"
+        className="bg-gradient-to-b from-redorange to-orange bg-clip-text text-2xl font-bold text-transparent dark:from-blue dark:to-skyblue"
       >
         Owing.
       </Link>
@@ -20,17 +20,17 @@ const RightHeader: React.FC = () => {
     <nav className="flex items-center space-x-12">
       <Link
         to="/"
-        className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text font-bold text-transparent"
+        className="bg-gradient-to-b from-redorange to-orange bg-clip-text font-bold text-transparent dark:from-blue dark:to-skyblue"
       >
         How to use
       </Link>
       <Link
         to="/"
-        className="from-redorange to-orange dark:from-blue dark:to-skyblue bg-gradient-to-b bg-clip-text font-bold text-transparent"
+        className="bg-gradient-to-b from-redorange to-orange bg-clip-text font-bold text-transparent dark:from-blue dark:to-skyblue"
       >
         Login
       </Link>
-      <button className="from-redorange to-orange dark:from-blue dark:to-skyblue rounded-full bg-gradient-to-r px-6 py-2 font-bold text-white">
+      <button className="rounded-full bg-gradient-to-r from-redorange to-orange px-6 py-2 font-bold text-darkblack dark:from-blue dark:to-skyblue">
         Contact us
       </button>
     </nav>
