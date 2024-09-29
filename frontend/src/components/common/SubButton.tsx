9814720c4b79d46fa88826358e974f4253201c10
hoshogi <hoshogi@gmail.com>
2024-09-29 21:30:32 +0900

    #17 [feat] : Button 컴포넌트 수정
    - width, height 수정

--- frontend/src/components/common/SubButton.tsx
+++ frontend/src/components/common/SubButton.tsx
@@ -6,7 +6,7 @@ interface SubButtonProps {
 const SubButton = ({ value, onClick }: SubButtonProps) => {
   return (
     <button
-      className="bg-verylightgray rounded-xl px-6 py-2 font-medium text-redorange"
+      className="bg-verylightgray h-[50px] w-full rounded-xl px-6 py-2 font-medium text-redorange"
       onClick={onClick}
     >
       {value}
