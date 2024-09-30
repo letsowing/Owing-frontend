43a499237652c992287fd84cf1d5f303d70091af
minju412 <ln8847@ajou.ac.kr>
2024-09-30 09:46:26 +0900

    #29 refactor: 원고 리스트를 상수로 선언

--- frontend/src/stores/dndStore.ts
+++ frontend/src/stores/dndStore.ts
@@ -1,11 +1,13 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware'; // persist를 이용해 로컬 스토리지에 저장되고, 페이지 새로고침 시에도 유지
+// persist를 이용해 로컬 스토리지에 저장되고, 페이지 새로고침 시에도 유지
+import { SCENARIO_LIST } from '@constants/scenarioList'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 // items 하나의 타입 정의
 interface DndItem {
-  id: string;
-  name: string;
-  description: string;
+  id: string
+  name: string
+  description: string
 }
 // interface DndItem {
 //   id?: string;
@@ -16,8 +18,8 @@ interface DndItem {
 
 // zustand store의 타입 정의
 interface DndState {
-  items: DndItem[];
-  moveItem: (dragIndex: number, hoverIndex: number) => void;
+  items: DndItem[]
+  moveItem: (dragIndex: number, hoverIndex: number) => void
 }
 
 // store 생성 (create 함수 사용)
@@ -28,16 +30,11 @@ export const useDndStore = create(
       // items는 초기값 설정
 
       // 파일리스트 예시
-      items: [
-        { id: '1', name: 'Item 1', description: 'This is item 1' },
-        { id: '2', name: 'Item 2', description: 'This is item 2' },
-        { id: '3', name: 'Item 3', description: 'This is item 3' },
-        { id: '4', name: 'Item 4', description: 'This is item 4' },
-        { id: '5', name: 'Item 5', description: 'This is item 5' },
-        { id: '6', name: 'Item 6', description: 'This is item 6' },
-        { id: '7', name: 'Item 7', description: 'This is item 7' },
-        { id: '8', name: 'Item 8', description: 'This is item 8' },
-      ],
+      items: SCENARIO_LIST.map((item) => ({
+        id: item.id,
+        name: item.name,
+        description: item.description,
+      })),
 
       // 폴더리스트 예시
       // items: [
@@ -55,14 +52,14 @@ export const useDndStore = create(
       // moveItem 드래그앤드롭시 발동되는 함수
       moveItem: (dragIndex: number, hoverIndex: number) =>
         set((state) => {
-          const updateItems = [...state.items];
-          const [draggedItem] = updateItems.splice(dragIndex, 1); // 드래그한 아이템 제거
-          updateItems.splice(hoverIndex, 0, draggedItem); // 드래그한 위치로 이동
-          return { items: updateItems };
+          const updateItems = [...state.items]
+          const [draggedItem] = updateItems.splice(dragIndex, 1) // 드래그한 아이템 제거
+          updateItems.splice(hoverIndex, 0, draggedItem) // 드래그한 위치로 이동
+          return { items: updateItems }
         }),
     }),
     {
       name: 'dnd-items', // 로컬 스토리지에 저장할 키
-    }
-  )
-);
+    },
+  ),
+)
