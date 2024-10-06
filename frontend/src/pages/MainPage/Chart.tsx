2ce51276c0dd0262d18155111db442a0716d3217
hoshogi <hoshogi@gmail.com>
2024-10-07 04:04:12 +0900

    #32 [feat] : 메인 페이지 Dashboard 컴포넌트 설계
    - Chart 설계

---
+++ frontend/src/pages/MainPage/Chart.tsx
@@ -0,0 +1,51 @@
+import ApexCharts from 'react-apexcharts'
+
+interface ChartProps {
+  wordCountStats: { day: Date; wordCount: number }[]
+}
+
+const Chart = ({ wordCountStats }: ChartProps) => {
+  return (
+    <ApexCharts
+      type="line"
+      series={[
+        { name: '글자수', data: wordCountStats.map((stat) => stat.wordCount) },
+      ]}
+      options={{
+        chart: {
+          height: 300,
+          width: 300,
+          toolbar: { show: false },
+          background: 'white',
+        },
+        stroke: {
+          curve: 'smooth',
+          width: 4,
+        },
+        grid: { show: true },
+        yaxis: { show: true },
+        xaxis: {
+          labels: { show: true },
+          axisTicks: { show: false },
+          axisBorder: { show: true },
+          categories: wordCountStats.map((stat) => stat.day.getTime()),
+          type: 'datetime',
+        },
+        title: {
+          text: 'Activity',
+          align: 'left',
+        },
+        fill: {
+          type: 'gradient',
+          gradient: { gradientToColors: ['#FB5D2B'], stops: [0, 100] },
+        },
+        colors: ['#FB5D2B'],
+        tooltip: {
+          y: { formatter: (value: number) => `${value}` },
+        },
+      }}
+    />
+  )
+}
+
+export default Chart
---
+++ frontend/src/pages/MainPage/Dashboard.tsx
@@ -0,0 +1,44 @@
+import Chart from './Chart'
+
+interface DashboardProps {
+  todayWordCount: number
+  monthTotalWordCount: number
+  monthAvgWordCount: number
+  wordCountStats: { day: Date; wordCount: number }[]
+}
+
+const Dashboard = ({
+  todayWordCount,
+  monthTotalWordCount,
+  monthAvgWordCount,
+  wordCountStats,
+}: DashboardProps) => {
+  return (
+    <div className="flex h-[430px] w-[320px] flex-col gap-3 rounded-3xl bg-beige">
+      <label className="mx-5 mb-3 mt-5 text-2xl font-semibold">Dashboard</label>
+      <div className="mx-5 flex justify-between">
+        <label className="text-gray">오늘의 활동 글자수</label>
+        <label className="font-semibold text-redorange">
+          {todayWordCount}자
+        </label>
+      </div>
+      <div className="mx-5 flex justify-between">
+        <label className="text-gray">이번 달 총 글 자수</label>
+        <label className="font-semibold text-redorange">
+          {monthTotalWordCount}자
+        </label>
+      </div>
+      <div className="mx-5 flex justify-between">
+        <label className="text-gray">이번 달 하루 평균 글자수</label>
+        <label className="font-semibold text-redorange">
+          {monthAvgWordCount}자
+        </label>
+      </div>
+      <div className="mx-3 mt-5">
+        <Chart wordCountStats={wordCountStats} />
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
