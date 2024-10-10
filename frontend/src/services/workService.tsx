762e14a0b854cf79742f3f7e8efcd267a35bb85a
hoshogi <hoshogi@gmail.com>
2024-10-10 14:38:20 +0900

    #53 [feat] : 프로젝트 리스트 조회 API 호출 구현

--- frontend/src/services/workService.tsx
+++ frontend/src/services/workService.tsx
@@ -2,6 +2,29 @@ import axiosInstance from '@utils/httpCommons'
 
 import { Work } from '@types'
 
+export const createWork = async (
+  title: string,
+  description: string,
+  category: string,
+  genres: string[],
+  imageUrl: string,
+): Promise<{ id: number; name: string }> => {
+  try {
+    const payload = {
+      title,
+      description,
+      category,
+      genres,
+      imageUrl,
+    }
+    const response = await axiosInstance.post('/project', payload)
+    return response.data
+  } catch (error) {
+    console.error('프로젝트 생성 실패:', error)
+    throw error
+  }
+}
+
 export const getAllProject = async (): Promise<Work[]> => {
   try {
     const response = await axiosInstance.get('/project/load')
