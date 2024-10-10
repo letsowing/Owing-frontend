ae1ee86793eba9040f150197ba040fdfb916f93f
hoshogi <hoshogi@gmail.com>
2024-10-10 09:55:56 +0900

    #49 [feat] : ProjectTagField 컴포넌트 설계

---
+++ frontend/src/pages/MainPage/modal/ProjectTagField.tsx
@@ -0,0 +1,48 @@
+import Tag from '@components/common/Tag'
+
+import { Work } from '@/types'
+
+interface ProjectTagFieldProps {
+  labelValue: string
+  tagList: readonly {
+    value: string
+    name: string
+  }[]
+  isEditable: boolean
+  work: Work
+  onTagClick?: () => void
+}
+
+const ProjectTagField = ({
+  labelValue,
+  tagList,
+  isEditable,
+  work,
+  onTagClick,
+}: ProjectTagFieldProps) => {
+  return (
+    <div className="flex flex-col">
+      <label className="font-semibold text-darkgray dark:text-coldbeige">
+        {labelValue}
+        {isEditable && (
+          <span className="ml-1 text-redorange dark:text-blue">*</span>
+        )}
+      </label>
+      <div className="mt-2 flex flex-wrap gap-3">
+        {tagList.map((tag) => (
+          <Tag
+            name={tag.name}
+            value={tag.value}
+            key={tag.name}
+            onClick={onTagClick}
+            isSelected={
+              work.genre === tag.value || work.category.includes(tag.value)
+            }
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ProjectTagField
