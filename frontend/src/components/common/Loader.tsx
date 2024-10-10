6236ab1d65590b08aa7e7fc21f5843c12e0b01a3
hoshogi <hoshogi@gmail.com>
2024-10-11 03:14:56 +0900

    #53 [feat] : Loader 구현

---
+++ frontend/src/components/common/Loader.tsx
@@ -0,0 +1,13 @@
+import '@assets/common/Loader.css'
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+    </div>
+  )
+}
+
+export default Loader
