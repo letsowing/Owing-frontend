8d9fee9b8ac6e4e2c6cd1d37659c3113b764edee
한소연 <chos9715@naver.com>
2024-10-11 12:32:27 +0900

    Update DnDWrapper.tsx

--- frontend/src/components/dnd/DnDWrapper.tsx
+++ frontend/src/components/dnd/DnDWrapper.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useDnd } from '@hooks/useDnd'
-import useNavigation from '@hooks/useNavigation'
 
 import DraggableBox from './DraggableBox'
 
