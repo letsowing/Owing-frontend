22a32481ba90bd2bd85fc1fbc304b5f01b63c193
hoshogi <hoshogi@gmail.com>
2024-11-12 09:24:03 +0900

    #43 [feat] : Presigned URL 이미지 업로드 구현

--- src/services/s3Service.ts
+++ src/services/s3Service.ts
@@ -1,35 +1,14 @@
-import axios from 'axios';
+import axios from 'axios'
 
-export const putUploadImageToS3 = async (
-  presignedUrl: string,
-  base64Image: string,
-) => {
+export const putUploadImageToS3 = async (presignedUrl: string, file: File) => {
   try {
-    let base64Data = base64Image;
-    let contentType = 'application/octet-stream';
-
-    if (base64Image.startsWith('data:')) {
-      const parts = base64Image.split(',');
-      contentType = parts[0].split(':')[1].split(';')[0];
-      base64Data = parts[1];
-    }
-
-    const binaryString = atob(base64Data);
-    const len = binaryString.length;
-    const bytes = new Uint8Array(len);
-    for (let i = 0; i < len; i++) {
-      bytes[i] = binaryString.charCodeAt(i);
-    }
-
-    const response = await axios.put(presignedUrl, bytes, {
+    await axios.put(presignedUrl, file, {
       headers: {
-        'Content-Type': contentType,
-        'Content-Disposition': 'inline',
+        'Content-Type': file.type,
       },
-    });
-    return response;
+    })
   } catch (error) {
-    console.error('S3 이미지 업로드 실패:', error);
-    throw error;
+    console.error('S3 이미지 업로드 실패')
+    throw error
   }
-};
+}
