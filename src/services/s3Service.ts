3821304f8aaca5ed3e8898e4a128963d49c68526
hoshogi <hoshogi@gmail.com>
2024-11-12 15:30:03 +0900

    #46 [feat] : PresignedUrl 업로드 API 수정
    - base64 이미지를 Blob 타입으로 변경후 업로드

--- src/services/s3Service.ts
+++ src/services/s3Service.ts
@@ -1,14 +1,32 @@
 import axios from 'axios'
 
-export const putUploadImageToS3 = async (presignedUrl: string, file: File) => {
+export const putUploadImageToS3 = async (
+  presignedUrl: string,
+  base64Image: string,
+) => {
   try {
-    await axios.put(presignedUrl, file, {
+    const imageBlob = base64ToBlob(base64Image)
+    await axios.put(presignedUrl, imageBlob, {
       headers: {
-        'Content-Type': file.type,
+        'Content-Type': imageBlob.type,
       },
     })
   } catch (error) {
     console.error('S3 이미지 업로드 실패')
     throw error
   }
+}
+
+const base64ToBlob = (base64: string): Blob => {
+  const parts = base64.split(';base64,')
+  const contentType = parts[0].split(':')[1]
+  const raw = window.atob(parts[1])
+  const rawLength = raw.length
+  const uInt8Array = new Uint8Array(rawLength)
+
+  for (let i = 0; i < rawLength; ++i) {
+    uInt8Array[i] = raw.charCodeAt(i)
+  }
+
+  return new Blob([uInt8Array], { type: contentType })
 }
